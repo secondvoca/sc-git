a672c46910977d22fda34c829439282969bdbbed
zisei <secondvoca@gmail.com>
2022-05-16 20:51:36 +0900

    prison 3-2

--- hello.py
+++ hello.py
@@ -11,6 +11,10 @@ It's gonna be fine, man.
 You're on the right side.
 bring him in.
 You can just call me friend.
+You have no jecket.
+Good looking one's always taken.
+
+3 way merge
 '''
 
 print(msg)
